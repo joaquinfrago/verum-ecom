99fb47cc1e56bfd2d857ba222ee035f511794987
joaquinfrago <gjfrago@gmail.com>
2020-09-09 18:28:01 +0800

    Updates

---
+++ precache-manifest.0b34bd832a3059d1b8458b9efec16da1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b590f14882ef4eb1726ab15689d482",
+    "url": "/verum-ecom/index.html"
+  },
+  {
+    "revision": "a4cce198cc3380fdf2d7",
+    "url": "/verum-ecom/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "4f4d84ec06d3c6606a93",
+    "url": "/verum-ecom/static/css/main.c038e7e8.chunk.css"
+  },
+  {
+    "revision": "a4cce198cc3380fdf2d7",
+    "url": "/verum-ecom/static/js/2.b8479e25.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/verum-ecom/static/js/2.b8479e25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4d84ec06d3c6606a93",
+    "url": "/verum-ecom/static/js/main.4e8d39f3.chunk.js"
+  },
+  {
+    "revision": "b84269d731ac084fd54c",
+    "url": "/verum-ecom/static/js/runtime-main.8f223aa1.js"
+  }
+]);
